1d5b90e16b718f35c34fe51bf9a99bb17ff26a86
Daniel <daniel4592@hotmail.com>
2016-12-28 21:43:58 +0000

    Added more words.  Added editable team names.  Seconds now start at 60 on page load.

--- charades.js
+++ charades.js
@@ -1,64 +1,80 @@
-var words = ["chair", "door", "house"];
-$("#next").click(function() {
-    $(this).siblings("p").text(words[Math.floor(Math.random()*3)]);
-}).click();
+$(document).ready(function() {
+    var words = ["chair", "door", "house", "monkey", "reimbursement", "baby", "old", "cat", "dog", "snail"];
+    $("#next").click(function() {
+        $(this).siblings("p").text(words[Math.floor(Math.random() * (words.length + 1))]);
+    }).click();
 
-$("#hide").click(function() {
-    var p = $(this).siblings("p");
-    if ($(p).css("visibility") === "visible") {
-        $(p).css("visibility", "hidden");
-        $(this).text("Show");
+    $("#hide").click(function() {
+        var p = $(this).siblings("p");
+        if ($(p).css("visibility") === "visible") {
+            $(p).css("visibility", "hidden");
+            $(this).text("Show");
+        }
+        else {
+            $(p).css("visibility", "visible");
+            $(this).text("Hide");
+        }
+    });
+
+    function teamEdit() {
+        var name = $(this).text();
+        var editableName = $("<input type='text' maxlength='15' />");
+        $(this).replaceWith(editableName);
+        editableName.val(name);
+        editableName.focus();
+        editableName.blur(function() {
+            var teamName = $("<h4 class='teamName'>" + $(this).val() + "</h4>");
+            $(this).replaceWith(teamName).click(teamEdit);
+            $(".teamName").click(teamEdit);
+        });
     }
-    else {
-        $(p).css("visibility", "visible");
-        $(this).text("Hide");
+    $(".teamName").click(teamEdit);
+    $(".scorePlus").click(function () {
+        var p = $(this).siblings("p");
+        var i = parseInt($(p).text(), 10);
+        $(p).text(i + 1);
+    });
+    $(".scoreMinus").click(function () {
+        var p = $(this).siblings("p");
+        var i = parseInt($(p).text(), 10);
+        if (i > 0)
+            $(p).text(i - 1);
+    });
+    var timer = 0;
+
+    function countdown() {
+        var seconds = $("#seconds");
+        var i = parseInt($(seconds).text(), 10);
+        if (i > 0)
+            $(seconds).text(i - 1);
+        else
+            countdownStop();
     }
-});
 
-$(".scorePlus").click(function () {
-    var p = $(this).siblings("p");
-    var i = parseInt($(p).text(), 10);
-    $(p).text(i + 1);
-});
-$(".scoreMinus").click(function () {
-    var p = $(this).siblings("p");
-    var i = parseInt($(p).text(), 10);
-    if (i > 0)
-        $(p).text(i - 1);
-});
-var timer = 0;
+    function countdownStop() {
+        if (timer) {
+            clearInterval(timer);
+            timer = false;
+            $("#start").text("Start");
+        }
+    }
 
-function countdown() {
-    var seconds = $("#seconds");
-    var i = parseInt($(seconds).text(), 10);
-    if (i > 0)
-        $(seconds).text(i - 1);
-    else
+    $("#start").click(function() {
+        if (!timer) {
+            timer = setInterval(countdown, 1000);
+            $("#start").text("Stop");
+        }
+        else
+            countdownStop();
+    });
+    $("#reset").click(function() {
         countdownStop();
-}
-
-function countdownStop() {
-    if (timer) {
-        clearInterval(timer);
-        timer = false;
-        $("#start").text("Start");
-    }
-}
-
-$("#start").click(function() {
-    if (!timer) {
-        timer = setInterval(countdown, 1000);
-        $("#start").text("Stop");
-    }
-    else
-        countdownStop();
-});
-$("#reset").click(function() {
-    countdownStop();
-    $(this).siblings("p").text($("#timerDuration").val());
-}).click();
-$("#timerDuration").change(function() {
-    if (!timer) {
-        $(this).siblings("div").children("p").text($(this).val());
-    }
+        $("#seconds").text($("#timerDuration").val());
+    }).click();
+    $("#timerDuration").change(function() {
+        if (!timer) {
+            $("#seconds").text($(this).val());
+        }
+    });
+    $("#seconds").text("60");
 });
